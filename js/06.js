6408901d484f16fc815a8a67171440ae666bcc5b
aelis <delahoz_ep@hotmail.com>
2025-02-25 11:17:25 -0400

    Martes ++

--- js/06.js
+++ js/06.js
@@ -1,1 +1,16 @@
 //El objeto Math
+
+let resultado;
+
+resultado = Math.PI; //Pi
+resultado = Math.round(2.3); //Redondeo
+resultado = Math.ceil(2.1); //Redondeo hacia arriba
+resultado = Math.floor(2.9);  //Redondeo hacia abajo
+resultado = Math.sqrt(144); //Raiz cuadrada
+resultado = Math.abs(-200); //Cambiar a positivo
+resultado = Math.min(3, 5, 7, 10, 25, 30); //Valor menor
+resultado = Math.max(3, 5, 7, 10, 25, 30); //Valor mayor
+resultado = Math.random();
+resultado = Math.floor( Math.random() * 30);
+
+console.log(resultado);
---
+++ js/07.js
@@ -0,0 +1,29 @@
+//Operaciones y su orden
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (600 + 600) * 1.05;
+
+//En js aplicamos las mimsas normas de operabilidad que en matematicas
+//console.log(resultado):
+
+//Incrementos
+
+let puntaje = 10;
+
+//Incrementos en 1
+
+puntaje++; //11
+++puntaje; //12
+
+//Decrementos en 1
+
+puntaje--; //11
+
+//Incremento en un valor especifico
+puntaje += 10; //11+10 = 21
+
+console.log(puntaje);
+
