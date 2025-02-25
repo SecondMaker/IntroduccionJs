9c3b7b7f0622b8383e39bcbbb28aafb800f12d31
aelis <delahoz_ep@hotmail.com>
2025-02-24 22:30:37 -0400

    Lunes commit

---
+++ js/01.js
@@ -0,0 +1,15 @@
+//Variables
+
+var producto = "Audifonos gamer"; //Inicializacion y asignacion de valor en una variable
+
+var disponible; //Inicializacion de variable sin valor
+
+producto = true; //Reasignacion de valor
+
+disponible = true; //Asignacion de valor
+
+var producto1 = "Computadora",
+    disponible1= "Mouse",
+    venta1 = "Teclado"; //Inicializacion multiple
+
+//Las variables no pueden iniciar con numeros o guiones y son sensitivas a estar en mayusculas o en minusculas
